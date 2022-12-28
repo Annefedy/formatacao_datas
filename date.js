08fbe7a95f631ef04deb09e86eab069b19544171
Annefedy <annecarine1997@gmail.com>
2022-12-28 00:14:33 -0300

    formatação de datas

---
+++ date.js
@@ -0,0 +1,5 @@
+let data = new Date();
+let dataFormatada = ((data.getDate() )) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear(); 
+console.log(dataFormatada);
+
+
---
+++ formatDate.js
@@ -0,0 +1,3 @@
+let data = new Date;
+let dataFormatada = (data.getFullYear() + "/" + ((data.getMonth() + 1)) + "/" + (data.getDate() )) ;                 
+console.log(dataFormatada);
